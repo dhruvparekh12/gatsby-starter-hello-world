3bdd10372096c863c10b5f4eca8bcb1848284074
Dhruv Parekh <parekhdhruv04@gmail.com>
2022-02-15 00:16:43 +0530

    Add cloud functions

---
+++ functions/demo1/test/hello4.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v2.0! Your executed! Test hello3",
+        randomNum: Math.floor(Math.random()*100).toString(),
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
